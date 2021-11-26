9a605ce081375be96e9b044594540d7966d3e2a6
godwinmat <67100329+godwinmat@users.noreply.github.com>
2021-11-26 12:21:24 +0100

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -141,6 +141,4 @@ client.on("messageCreate", async (message) => {
 		}
 	}
 });
-// const mySecret = process.env['DISCORDJS_BOT_TOKEN']
-// console.log(mySecret)
-client.login("OTEzMzUzMjk2MzIyOTA4MTcw.YZ9Qiw.NvExxAt15Cn5mgOiZHR2lQeerFU");
+client.login(process.env.DISCORDJS_BOT_TOKEN);
